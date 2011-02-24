3109b42b564faa358c0c2d34f6dd9542d69c3b49
david.baird@synergi.com.au <david.baird@synergi.com.au@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-02-24 22:09:47 +0000

    fixed rtf test
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@31 dd429c29-c832-59a0-e864-46e68368aa0b

--- delta-editor/src/test/java/au/org/ala/delta/rtf/RTFReaderTests.java
+++ delta-editor/src/test/java/au/org/ala/delta/rtf/RTFReaderTests.java
@@ -1,16 +1,15 @@
 package au.org.ala.delta.rtf;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 import junit.framework.TestCase;
 
 public class RTFReaderTests extends TestCase {
 
 	public void testReader1() throws IOException {
-		FileInputStream fis = new FileInputStream("c:/zz/test2.rtf");
-		
-		RTFReader reader = new RTFReader(fis);
+		URL url = getClass().getResource("/rtf/test1.rtf");
+		RTFReader reader = new RTFReader(url.openStream());
 		reader.parse();
 	}
 
