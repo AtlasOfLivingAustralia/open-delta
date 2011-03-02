0dd9690eaaf2778a608331d5ba05e032cf048189
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-03-02 05:28:29 +0000

    Fixes for About dialog. Can now view Mozilla license information, and copy version information to the clipboard.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@74 dd429c29-c832-59a0-e864-46e68368aa0b

--- delta-editor/src/main/java/au/org/ala/delta/AboutBox.java
+++ delta-editor/src/main/java/au/org/ala/delta/AboutBox.java
@@ -1,8 +1,12 @@
 package au.org.ala.delta;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -28,40 +32,35 @@ public class AboutBox extends JDialog {
 		
 		this.setMinimumSize(new Dimension(500, 200));
 		
-		//Common attributes for our different text areas
-		SimpleAttributeSet aSet = new SimpleAttributeSet();
-		StyleConstants.setFontFamily(aSet, "Tahoma");
-		StyleConstants.setFontSize(aSet, 16);
-		StyleConstants.setAlignment(aSet, StyleConstants.ALIGN_CENTER);
-		
 		JPanel topPanel = new JPanel();
+				
+		String topText = "<html><center>" +
+				"Delta Editor<br>" +
+				"Version " + getVersionFromManifest() +
+				"</center></html>";
 		
-		JTextPane topTextPane = new JTextPane();		
-		String topText = "Delta Editor\n" +
-		"Version " + getVersionFromManifest();
-		topTextPane.setText(topText);
-		topTextPane.setEnabled(false);
-		topTextPane.setBackground(this.getBackground());
-		topTextPane.setFont(this.getFont());
-		topTextPane.getStyledDocument().setParagraphAttributes(0, topText.length(), aSet, true);
+		JLabel topTextLabel = new JLabel(topText);
+		topTextLabel.setFont(new Font(topTextLabel.getFont().getName(), topTextLabel.getFont().getStyle(), 16));
 		
 		Icon deltaIcon = IconHelper.createLargeIcon();
 		JLabel iconLabel = new JLabel(deltaIcon);
 		iconLabel.setBorder(BorderFactory.createEmptyBorder(0,0,0,20));
 		
 		topPanel.add(iconLabel, BorderLayout.EAST);
-		topPanel.add(topTextPane, BorderLayout.CENTER);
+		topPanel.add(topTextLabel, BorderLayout.CENTER);
 		
 
-		JTextPane middleTextPane = new JTextPane();
-		String middleText = "Based on work by M.J. Dallwitz, T.A. Paine and E.J. Zurcher\n" +
-		"Copyright Atlas of Living Australia 2011";
-		middleTextPane.setText(middleText);
-		middleTextPane.setEnabled(false);
-		middleTextPane.setBackground(this.getBackground());
-		middleTextPane.setFont(this.getFont());
-		middleTextPane.getStyledDocument().setParagraphAttributes(0, middleText.length(), aSet, true);
+		String middleText = "<html><center>" +
+			"Part of the Open Delta Suite<br>" +
+			"Based on work by M.J. Dallwitz, T.A. Paine and E.J. Zurcher<br>" +
+			"Copyright Atlas of Living Australia 2011<br>" +
+			"</center></html>";
 		
+		JLabel middleTextLabel = new JLabel(middleText);
+		middleTextLabel.setFont(new Font(middleTextLabel.getFont().getName(), middleTextLabel.getFont().getStyle(), 16));
+		
+		JPanel middlePanel = new JPanel();
+		middlePanel.add(middleTextLabel, BorderLayout.CENTER);
 		
 		JButton okButton = new JButton("OK");
 		okButton.addActionListener(new ActionListener() {
@@ -73,12 +72,44 @@ public class AboutBox extends JDialog {
 			
 		});
 		
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(okButton, BorderLayout.CENTER);
+		JButton licenseButton = new JButton("License Details");
+		licenseButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				LicenseInfoBox licenseInfoBox = new LicenseInfoBox(AboutBox.this);
+				licenseInfoBox.setVisible(true);
+			}
+			
+		});
+		
+		JButton copyVersionButton = new JButton("Copy Version Info To Clipboard");
+		copyVersionButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				copyVersionInfoToClipboard();
+			}
+			
+		});
+		
+		JPanel leftButtonPanel = new JPanel();
+		JPanel rightButtonPanel = new JPanel();
+		
+		leftButtonPanel.add(licenseButton, BorderLayout.EAST);
+		leftButtonPanel.add(copyVersionButton, BorderLayout.WEST);
+		leftButtonPanel.setBorder(BorderFactory.createEmptyBorder(0,0,0,20));
+		
+		rightButtonPanel.add(okButton, BorderLayout.CENTER);
+		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.add(leftButtonPanel, BorderLayout.EAST);
+		bottomPanel.add(rightButtonPanel, BorderLayout.WEST);
+
 		
 		this.add(topPanel, BorderLayout.NORTH);
-		this.add(middleTextPane, BorderLayout.CENTER);
-		this.add(buttonPanel, BorderLayout.SOUTH);
+		this.add(middlePanel, BorderLayout.CENTER);
+		this.add(bottomPanel, BorderLayout.SOUTH);
 		
 		//center the dialog on screen
 		this.setLocationRelativeTo(owner);
@@ -88,5 +119,35 @@ public class AboutBox extends JDialog {
 	private String getVersionFromManifest() {
 		String versionString = getClass().getPackage().getImplementationVersion();
 		return versionString;
+	}
+	
+	private void copyVersionInfoToClipboard() {
+		StringBuilder versionInfo = new StringBuilder();
+		versionInfo.append("Delta Editor " + getVersionFromManifest());
+		versionInfo.append("\n");
+		versionInfo.append("java.version: ");
+		versionInfo.append(System.getProperty("java.version"));
+		versionInfo.append("\n");
+		versionInfo.append("java.vendor: ");
+		versionInfo.append(System.getProperty("java.vendor"));
+		versionInfo.append("\n");
+		versionInfo.append("os.name: ");
+		versionInfo.append(System.getProperty("os.name"));
+		versionInfo.append("\n");
+		versionInfo.append("os.arch: ");
+		versionInfo.append(System.getProperty("os.arch"));
+		versionInfo.append("\n");
+		versionInfo.append("os.version: ");
+		versionInfo.append(System.getProperty("os.version"));
+		versionInfo.append("\n");
+		versionInfo.append("user.language: ");
+		versionInfo.append(System.getProperty("user.language"));
+		versionInfo.append("\n");
+		versionInfo.append("user.region: ");
+		versionInfo.append(System.getProperty("user.region"));
+		
+		StringSelection selection = new StringSelection(versionInfo.toString());
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+		
 	}
 }
---
+++ delta-editor/src/main/java/au/org/ala/delta/LicenseInfoBox.java
@@ -0,0 +1,88 @@
+package au.org.ala.delta;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+
+public class LicenseInfoBox extends JDialog {
+
+	public LicenseInfoBox(Dialog owner) {
+		super(owner, "License Information",  true);
+		
+		this.setMinimumSize(new Dimension(800, 800));
+		
+		String labelText = "<html><center>This software is made available under Version 1.1 of the Mozilla Public License<br>" +
+			"Source code can be downloaded from: http://code.google.com/p/open-delta</center></html>";
+		
+		JLabel topLabel = new JLabel(labelText);
+		topLabel.setFont(new Font(topLabel.getFont().getName(), topLabel.getFont().getStyle(), 14));
+		topLabel.setBorder(BorderFactory.createEmptyBorder(20,0,20,0));
+
+		JPanel topPanel = new JPanel();
+		topPanel.add(topLabel, BorderLayout.CENTER);
+		
+		JTextArea textArea = new JTextArea(loadLicenseText());
+		textArea.setEnabled(false);
+		textArea.setDisabledTextColor(Color.BLACK);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED); 
+		
+		
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				LicenseInfoBox.this.dispose();
+			}
+			
+		});
+		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.PAGE_AXIS));
+		okButton.setAlignmentX(RIGHT_ALIGNMENT);
+		bottomPanel.add(okButton, BorderLayout.WEST);
+		bottomPanel.setBorder(BorderFactory.createEmptyBorder(20,0,20,20));
+		
+		this.add(topPanel, BorderLayout.NORTH);
+		this.add(scrollPane, BorderLayout.CENTER);
+		this.add(bottomPanel, BorderLayout.SOUTH);
+		
+		//center the dialog on screen
+		this.setLocationRelativeTo(owner);
+	}
+	
+	public String loadLicenseText() {
+		
+		StringBuilder licenseText = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(LicenseInfoBox.class.getResourceAsStream("/au/org/ala/delta/resources/MPL-1.1.txt")));
+		String line;
+		
+		try {
+			while ((line = in.readLine()) != null) {
+				licenseText.append(line);
+				licenseText.append("\n");
+			}
+		} catch (Exception ex) {
+			
+		}
+		
+		return licenseText.toString();
+	}
+}
