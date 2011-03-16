2937a8790c002d1cdcb4ba350064462fd3f72a82
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-03-16 07:06:52 +0000

    Added menu bar and properties file to Intkey
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@295 dd429c29-c832-59a0-e864-46e68368aa0b

--- intkey/src/main/java/au/org/ala/delta/intkey/Intkey.java
+++ intkey/src/main/java/au/org/ala/delta/intkey/Intkey.java
@@ -3,14 +3,23 @@ package au.org.ala.delta.intkey;
 import java.awt.Dimension;
 import java.awt.SystemColor;
 
+import javax.swing.ActionMap;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+import org.jdesktop.application.Action;
 import org.jdesktop.application.SingleFrameApplication;
+
+import au.org.ala.delta.ui.AboutBox;
+import au.org.ala.delta.ui.util.IconHelper;
 
 public class Intkey extends SingleFrameApplication {
     
-    JDesktopPane _desktop;
+    private JDesktopPane _desktop;
+    private ActionMap _actionMap;
     
     public static void main(String[] args) {
         launch(Intkey.class, args);
@@ -18,13 +27,18 @@ public class Intkey extends SingleFrameApplication {
 
     @Override
     protected void startup() {
+        _actionMap = getContext().getActionMap(this);
+        
         JFrame mainFrame = getMainFrame();
         mainFrame.setTitle("Intkey");
         mainFrame.setPreferredSize(new Dimension(800,600));
         mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        
+        mainFrame.setIconImages(IconHelper.getRedIconList());
+       
         _desktop = new JDesktopPane();
         _desktop.setBackground(SystemColor.control);
+        
+        getMainView().setMenuBar(buildMenus());
 
         show(_desktop);
     }
@@ -32,6 +46,59 @@ public class Intkey extends SingleFrameApplication {
     @Override
     protected void shutdown() {
         super.shutdown();
+    }
+    
+    private JMenuBar buildMenus() {
+        
+
+        JMenuBar menuBar = new JMenuBar();
+
+        // File menu
+        JMenu mnuFile = new JMenu();
+        mnuFile.setName("mnuFile");
+
+        JMenuItem mnuItFileExit = new JMenuItem();
+        mnuItFileExit.setAction(_actionMap.get("exitApplication"));
+
+        mnuFile.addSeparator();
+        mnuFile.add(mnuItFileExit);
+        menuBar.add(mnuFile);
+
+        // Help menu
+        JMenu mnuHelp = new JMenu();
+        mnuHelp.setName("mnuHelp");
+        JMenuItem mnuItHelpContents = new JMenuItem();
+        mnuItHelpContents.setName("mnuItHelpContents");
+        mnuHelp.add(mnuItHelpContents);
+        //mnuItHelpContents.addActionListener(_helpController.helpAction());
+        
+        JMenuItem mnuItHelpOnSelection = new JMenuItem(IconHelper.createImageIcon("help_cursor.png"));
+        mnuItHelpOnSelection.setName("mnuItHelpOnSelection");
+        
+        //mnuItHelpOnSelection.addActionListener(_helpController.helpOnSelectionAction());
+        mnuHelp.add(mnuItHelpOnSelection);
+
+        
+        JMenuItem mnuItAbout = new JMenuItem();
+        mnuItAbout.setAction(_actionMap.get("openAbout"));
+        
+        mnuHelp.addSeparator();
+        mnuHelp.add(mnuItAbout);
+        
+        menuBar.add(mnuHelp);
+        
+        return menuBar;
+    }
+    
+    @Action
+    public void exitApplication() {
+        exit();
+    }
+    
+    @Action
+    public void openAbout() {
+        AboutBox aboutBox = new AboutBox(getMainFrame());
+        show(aboutBox);
     }
 
 }
