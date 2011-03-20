5cf3a136605c389e4343d7eaa8e67411719df5f1
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-03-20 22:49:44 +0000

    Implemented the link characters directive.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@316 dd429c29-c832-59a0-e864-46e68368aa0b

---
+++ common/src/main/java/au/org/ala/delta/directives/AbstractCharacterSetDirective.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+package au.org.ala.delta.directives;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.math.IntRange;
+
+import au.org.ala.delta.DeltaContext;
+
+/**
+ * An AbstractCharacterSetDirective is a directive that takes a space separated list of 
+ * character sets of the form: c1:c2:...cn where cn is a character number or a range of numbers.
+ */
+public abstract class AbstractCharacterSetDirective extends Directive {
+
+	private static Pattern CHAR_SET_PATTERN = Pattern.compile("^(\\d+)[:-](.*)$");
+
+	public AbstractCharacterSetDirective(String... controlWords) {
+		super(controlWords);
+	}
+
+	@Override
+	public void process(DeltaContext context, String data) throws Exception {
+		String[] typeDescriptors = data.split(" |\\n");
+		for (String typeDescriptor : typeDescriptors) {
+			typeDescriptor = typeDescriptor.trim();
+			if (CHAR_SET_PATTERN.matcher(typeDescriptor).matches()) {
+				String[] bits = typeDescriptor.trim().split(":");
+				
+				Set<Integer> characters = new HashSet<Integer>();
+				for (String bit : bits) {
+					IntRange r = parseRange(bit);
+					for (int i : r.toArray()) {
+						characters.add(i);
+					}
+				}
+				
+				processCharacterSet(context, characters);
+			}
+			
+		}
+	}
+
+	protected abstract void processCharacterSet(DeltaContext context, Set<Integer> characters);
+
+}
