983072f827f6d331b2b3f352fd97903a127e82ba
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-04-21 05:56:36 +0000

    Implemented drag & drop of Items in the ItemList.  (the actual move/copy operations aren't implemented, the drag and drop currently just does a system.out.println).
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@510 dd429c29-c832-59a0-e864-46e68368aa0b

--- delta-editor/src/main/java/au/org/ala/delta/editor/ItemController.java
+++ delta-editor/src/main/java/au/org/ala/delta/editor/ItemController.java
@@ -1,10 +1,18 @@
 package au.org.ala.delta.editor;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import javax.swing.ActionMap;
+import javax.swing.DropMode;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
+import javax.swing.TransferHandler;
 
 import org.apache.commons.lang.StringUtils;
 import org.jdesktop.application.Action;
@@ -31,6 +39,10 @@ public class ItemController {
 		_view = view;
 		_model = model;
 		_context = Application.getInstance().getContext();
+		
+		_view.setDragEnabled(true);
+		_view.setDropMode(DropMode.INSERT);
+		_view.setTransferHandler(new ItemTransferHandler());
 		
 		buildPopup();
 	}
@@ -110,6 +122,92 @@ public class ItemController {
 		int result = MessageDialogHelper.showConfirmDialog(_view, "CONFIRM", 
 				"Please confirm that you really wish to delete this taxon:\n" + toDelete.getDescription(), 50);
 		return result == JOptionPane.OK_OPTION;
+	}
+	
+	private static DataFlavor _itemFlavor = new DataFlavor(Item.class, "Item");
+	
+	/**
+	 * Handles drag and drop of Items in the ItemList.
+	 */
+	class ItemTransferHandler extends TransferHandler {
+		
+		private static final long serialVersionUID = 889705892088002277L;
+		private int sourceIndex;
+		
+		public boolean canImport(TransferHandler.TransferSupport info) {
+			return info.isDataFlavorSupported(_itemFlavor);
+		}
+		
+		protected Transferable createTransferable(JComponent c) {
+			ItemList list = (ItemList)c;
+			Item item = list.getSelectedItem();
+			sourceIndex = list.getSelectedIndex();
+			
+			return new ItemTransferrable(item);
+			
+		}
+		
+		public int getSourceActions(JComponent c) {
+			return TransferHandler.COPY_OR_MOVE;
+		}
+		
+		public boolean importData(TransferHandler.TransferSupport info) {
+			ItemList list = (ItemList)info.getComponent();
+			
+			Transferable transferrable = info.getTransferable();
+			
+			Item item = null;
+			try {
+				item = (Item)transferrable.getTransferData(_itemFlavor);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+			
+			int targetIndex = list.getDropLocation().getIndex();
+			if (info.getUserDropAction() == DnDConstants.ACTION_MOVE) {
+				System.out.println("Moving item "+item.getItemNumber() + " from "+sourceIndex+ " to "+targetIndex);
+			}
+			else if (info.getUserDropAction() == DnDConstants.ACTION_COPY) {
+				System.out.println("Copying item "+item.getItemNumber() + " from "+sourceIndex+ " to "+targetIndex);
+				
+			}
+			return true;
+		}
+	}
+
+	/**
+	 * Transfers an Item.
+	 */
+	class ItemTransferrable implements Transferable {
+
+		private Item _item;
+		
+		public ItemTransferrable(Item item) {
+			_item = item;
+		}
+		
+		@Override
+		public DataFlavor[] getTransferDataFlavors() {
+			return new DataFlavor[] {_itemFlavor};
+		}
+
+		@Override
+		public boolean isDataFlavorSupported(DataFlavor flavor) {
+			
+			return _itemFlavor.equals(flavor);
+		}
+
+		@Override
+		public Object getTransferData(DataFlavor flavor)
+				throws UnsupportedFlavorException, IOException {
+			if (!isDataFlavorSupported(flavor)) {
+				throw new UnsupportedFlavorException(flavor);
+			}
+			return _item;
+		}
+		
 	}
 	
 }
