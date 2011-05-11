90bd73f1c0970b543e66657ec26cabf4c68586cd
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-05-11 02:18:40 +0000

    Refactored a parent class out of EditorDataModel.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@588 dd429c29-c832-59a0-e864-46e68368aa0b

---
+++ delta-editor/src/main/java/au/org/ala/delta/editor/ui/DataSetWrapper.java
@@ -0,0 +1,217 @@
+package au.org.ala.delta.editor.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import au.org.ala.delta.model.AbstractObservableDataSet;
+import au.org.ala.delta.model.Attribute;
+import au.org.ala.delta.model.Character;
+import au.org.ala.delta.model.CharacterType;
+import au.org.ala.delta.model.DeltaDataSet;
+import au.org.ala.delta.model.Item;
+import au.org.ala.delta.model.observer.DeltaDataSetChangeEvent;
+import au.org.ala.delta.model.observer.DeltaDataSetObserver;
+
+/**
+ * A wrapper for an observable DeltaDataSet.  It allows DeltaDataSetObservers to
+ * be isolated from the actual data set so that references to closed views don't 
+ * hang around in the model and prevent garbage collection.
+ */
+public class DataSetWrapper implements DeltaDataSet, DeltaDataSetObserver {
+
+	/** The data set we are wrapping */
+	protected AbstractObservableDataSet _wrappedDataSet;
+	/** Maintains a list of objects interested in being notified of changes to this model */
+	private List<DeltaDataSetObserver> _observerList = new ArrayList<DeltaDataSetObserver>();
+
+	public DataSetWrapper(AbstractObservableDataSet dataSet) {
+		_wrappedDataSet = dataSet;
+		_wrappedDataSet.addDeltaDataSetObserver(this);
+	}
+
+	@Override
+	public String getName() {
+		return _wrappedDataSet.getName();
+	}
+
+	@Override
+	public void setName(String name) {
+		_wrappedDataSet.setName(name);
+	}
+
+	@Override
+	public void close() {
+		_wrappedDataSet.close();
+	}
+	
+	/**
+	 * Adds an observer interested in receiving notification of changes to this model.
+	 * Duplicate observers are ignored.
+	 * @param observer the observer to add.
+	 */
+	public void addDeltaDataSetObserver(DeltaDataSetObserver observer) {
+		if (_observerList.contains(observer)) {
+			return;
+		}
+		_observerList.add(observer);
+	}
+
+	/**
+	 * Prevents an observer from receiving further notifications of changes to this model.
+	 * @param observer the observer to remove.
+	 */
+	public void removeDeltaDataSetObserver(DeltaDataSetObserver observer) {
+		_observerList.remove(observer);
+	}
+
+	@Override
+	public Item getItem(int number) {
+		Item item = _wrappedDataSet.getItem(number);
+		return item;
+	}
+
+	@Override
+	public String getAttributeAsString(int itemNumber, int characterNumber) {
+		return _wrappedDataSet.getAttributeAsString(itemNumber, characterNumber);
+	}
+
+	@Override
+	public Character getCharacter(int number) {
+		Character character = _wrappedDataSet.getCharacter(number);
+		return character;
+	}
+
+	@Override
+	public int getNumberOfCharacters() {
+		return _wrappedDataSet.getNumberOfCharacters();
+	}
+
+	@Override
+	public int getMaximumNumberOfItems() {
+		return _wrappedDataSet.getMaximumNumberOfItems();
+	}
+
+	@Override
+	public Character addCharacter(int characterNumber, CharacterType type) {
+		Character character = _wrappedDataSet.addCharacter(characterNumber, type);
+		return character;
+	}
+
+	@Override
+	public Character addCharacter(CharacterType type) {
+		Character character = _wrappedDataSet.addCharacter(type);
+		return character;
+	}
+
+	@Override
+	public Item addItem(int itemNumber) {
+		Item item = _wrappedDataSet.addItem(itemNumber);
+		return item;
+	}
+
+	@Override
+	public Item addItem() {
+		Item item = _wrappedDataSet.addItem();
+		return item;
+	}
+
+	@Override
+	public Item addVariantItem(int parentItemNumber, int itemNumber) {
+		return _wrappedDataSet.addVariantItem(parentItemNumber, itemNumber);
+	}
+
+	@Override
+	public boolean isModified() {
+		return _wrappedDataSet.isModified();
+	}
+
+	public void deleteItem(int itemNumber) {
+		deleteItem(_wrappedDataSet.getItem(itemNumber));
+	}
+
+	@Override
+	public void deleteItem(Item item) {
+		_wrappedDataSet.deleteItem(item);
+	}
+
+	@Override
+	public Attribute getAttribute(int itemNumber, int characterNumber) {
+		return _wrappedDataSet.getAttribute(itemNumber, characterNumber);
+	}
+
+	@Override
+	public void moveItem(Item item, int newItemNumber) {
+		_wrappedDataSet.moveItem(item, newItemNumber);
+	}
+
+	@Override
+	public void itemAdded(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).itemAdded(event);
+		}
+	}
+
+	@Override
+	public void itemDeleted(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).itemDeleted(event);
+		}
+	}
+
+	@Override
+	public void itemMoved(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).itemMoved(event);
+		}
+	}
+
+	@Override
+	public void itemEdited(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).itemEdited(event);
+		}
+	}
+
+	@Override
+	public void itemSelected(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).itemSelected(event);
+		}
+	}
+
+	@Override
+	public void characterAdded(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).characterAdded(event);
+		}
+	}
+
+	@Override
+	public void characterDeleted(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).characterDeleted(event);
+		}
+	}
+
+	@Override
+	public void characterMoved(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).characterMoved(event);
+		}
+	}
+
+	@Override
+	public void characterEdited(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).characterEdited(event);
+		}
+	}
+
+	@Override
+	public void characterSelected(DeltaDataSetChangeEvent event) {
+		for (int i=_observerList.size()-1; i>=0; i--) {
+			_observerList.get(i).characterSelected(event);
+		}	
+	}
+
+}
--- delta-editor/src/main/java/au/org/ala/delta/editor/ui/EditorDataModel.java
+++ delta-editor/src/main/java/au/org/ala/delta/editor/ui/EditorDataModel.java
@@ -3,17 +3,10 @@ package au.org.ala.delta.editor.ui;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import au.org.ala.delta.model.AbstractObservableDataSet;
-import au.org.ala.delta.model.Attribute;
 import au.org.ala.delta.model.Character;
-import au.org.ala.delta.model.CharacterType;
-import au.org.ala.delta.model.DeltaDataSet;
 import au.org.ala.delta.model.Item;
-import au.org.ala.delta.model.observer.DeltaDataSetChangeEvent;
-import au.org.ala.delta.model.observer.DeltaDataSetObserver;
 
 
 /**
@@ -22,11 +15,8 @@ import au.org.ala.delta.model.observer.DeltaDataSetObserver;
  * to allow clean removal of listeners from the backing DeltaDataSet when a view of the model is 
  * closed. 
  */
-public class EditorDataModel implements DeltaDataSet, DeltaDataSetObserver {
+public class EditorDataModel extends DataSetWrapper {
 
-	/** The currently selected data set */
-	private AbstractObservableDataSet _currentDataSet;
-	
 	/** The number of the currently selected character */
 	private Character _selectedCharacter;
 	
@@ -36,11 +26,8 @@ public class EditorDataModel implements DeltaDataSet, DeltaDataSetObserver {
 	/** Helper class for notifying interested parties of property changes */
 	private PropertyChangeSupport _propertyChangeSupport;
 	
-	/** Maintains a list of objects interested in being notified of changes to this model */
-	private List<DeltaDataSetObserver> _observerList = new ArrayList<DeltaDataSetObserver>();
-	
 	public EditorDataModel(AbstractObservableDataSet dataSet) {
-		setCurrentDataSet(dataSet);
+		super(dataSet);
 		_propertyChangeSupport = new PropertyChangeSupport(this);
 	}
 	
@@ -56,15 +43,6 @@ public class EditorDataModel implements DeltaDataSet, DeltaDataSetObserver {
 		}
 	}
 	
-	public void setCurrentDataSet(AbstractObservableDataSet dataSet) {
-		if (_currentDataSet != null) {
-			_currentDataSet.removeDeltaDataSetObserver(this);
-		}
-		_currentDataSet = dataSet;
-		_currentDataSet.addDeltaDataSetObserver(this);
-		_selectedCharacter = null;
-		_selectedItem = null;
-	}
 	
 	public void setSelectedItem(Item selectedItem) {
 		_selectedItem = selectedItem;
@@ -83,40 +61,28 @@ public class EditorDataModel implements DeltaDataSet, DeltaDataSetObserver {
 	}
 	
 	public AbstractObservableDataSet getCurrentDataSet() {
-		return _currentDataSet;
-	}
-	
-
-	/**
-	 * Adds an observer interested in receiving notification of changes to this model.
-	 * Duplicate observers are ignored.
-	 * @param observer the observer to add.
-	 */
-	public void addDeltaDataSetObserver(DeltaDataSetObserver observer) {
-		if (_observerList.contains(observer)) {
-			return;
-		}
-		_observerList.add(observer);
-	}
-
-	/**
-	 * Prevents an observer from receiving further notifications of changes to this model.
-	 * @param observer the observer to remove.
-	 */
-	public void removeDeltaDataSetObserver(DeltaDataSetObserver observer) {
-		_observerList.remove(observer);
+		return _wrappedDataSet;
 	}
 	
 	@Override
+	public void deleteItem(Item item) {
+		_wrappedDataSet.deleteItem(item);
+		if (_selectedItem != null && _selectedItem.equals(item)) {
+			_selectedItem = null;
+		}
+	}
+	
+
+	@Override
 	public String getName() {
-		String name = _currentDataSet.getName();
+		String name = _wrappedDataSet.getName();
 		if (name == null) {
 			name = "";
 		}
 		return name;
 	}
 	public String getShortName() {
-		String name = _currentDataSet.getName();
+		String name = _wrappedDataSet.getName();
 		if (name == null) {
 			name = "";
 		}
@@ -126,168 +92,14 @@ public class EditorDataModel implements DeltaDataSet, DeltaDataSetObserver {
 	
 	public void setName(String name) {
 		
-		_currentDataSet.setName(name);
+		_wrappedDataSet.setName(name);
 		
 		_propertyChangeSupport.firePropertyChange("name", null, name);
 	}
 
 	@Override
-	public Item getItem(int number) {
-		Item item = _currentDataSet.getItem(number);
-		return item;
-	}
-
-	@Override
-	public String getAttributeAsString(int itemNumber, int characterNumber) {
-		return _currentDataSet.getAttributeAsString(itemNumber, characterNumber);
-	}
-
-	@Override
-	public Character getCharacter(int number) {
-		Character character = _currentDataSet.getCharacter(number);
-		return character;
-	}
-
-	@Override
-	public int getNumberOfCharacters() {
-		return _currentDataSet.getNumberOfCharacters();
-	}
-
-	@Override
-	public int getMaximumNumberOfItems() {
-		return _currentDataSet.getMaximumNumberOfItems();
-	}
-	
-	@Override
 	public void close() {
-		_currentDataSet.removeDeltaDataSetObserver(this);
-		_currentDataSet.close();
-	}
-	
-	@Override
-	public Character addCharacter(int characterNumber, CharacterType type) {
-		Character character = _currentDataSet.addCharacter(characterNumber, type);
-		return character;
-	}
-	
-	@Override
-	public Character addCharacter(CharacterType type) {
-		Character character = _currentDataSet.addCharacter(type);
-		return character;
-	}
-
-	@Override
-	public Item addItem(int itemNumber) {
-		Item item = _currentDataSet.addItem(itemNumber);
-		return item;
-	}
-
-	@Override
-	public Item addItem() {
-		Item item = _currentDataSet.addItem();
-		return item;
-	}
-	
-	@Override
-	public Item addVariantItem(int parentItemNumber, int itemNumber) {
-		return _currentDataSet.addVariantItem(parentItemNumber, itemNumber);
-	}
-	
-	@Override
-	public boolean isModified() {
-		return _currentDataSet.isModified();
-	}
-	
-	public void deleteItem(int itemNumber) {
-		deleteItem(_currentDataSet.getItem(itemNumber));
-	}
-	
-	@Override
-	public void deleteItem(Item item) {
-		_currentDataSet.deleteItem(item);
-		if (_selectedItem != null && _selectedItem.equals(item)) {
-			_selectedItem = null;
-		}
-	}
-
-	@Override
-	public Attribute getAttribute(int itemNumber, int characterNumber) {
-		return _currentDataSet.getAttribute(itemNumber, characterNumber);
-	}
-	
-
-	@Override
-	public void moveItem(Item item, int newItemNumber) {
-		_currentDataSet.moveItem(item, newItemNumber);
-	}
-
-	@Override
-	public void itemAdded(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).itemAdded(event);
-		}
-	}
-
-	@Override
-	public void itemDeleted(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).itemDeleted(event);
-		}
-	}
-
-	@Override
-	public void itemMoved(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).itemMoved(event);
-		}
-	}
-
-	@Override
-	public void itemEdited(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).itemEdited(event);
-		}
-	}
-
-	@Override
-	public void itemSelected(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).itemSelected(event);
-		}
-	}
-
-	@Override
-	public void characterAdded(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).characterAdded(event);
-		}
-	}
-
-	@Override
-	public void characterDeleted(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).characterDeleted(event);
-		}
-	}
-
-	@Override
-	public void characterMoved(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).characterMoved(event);
-		}
-	}
-
-	@Override
-	public void characterEdited(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).characterEdited(event);
-		}
-	}
-
-	@Override
-	public void characterSelected(DeltaDataSetChangeEvent event) {
-		for (int i=_observerList.size()-1; i>=0; i--) {
-			_observerList.get(i).characterSelected(event);
-		}	
+		_wrappedDataSet.removeDeltaDataSetObserver(this);
+		_wrappedDataSet.close();
 	}
 }
