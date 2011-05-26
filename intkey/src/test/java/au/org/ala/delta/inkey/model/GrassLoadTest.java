de17503b6106c62b989a182e06fd9a86b0e557b6
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-05-26 07:52:35 +0000

    update pom files to include "win-installer" profile for windows launch4j executables and installer.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@685 dd429c29-c832-59a0-e864-46e68368aa0b

---
+++ intkey/src/test/java/au/org/ala/delta/inkey/model/GrassLoadTest.java
@@ -0,0 +1,15 @@
+package au.org.ala.delta.inkey.model;
+
+
+public class GrassLoadTest {
+
+    /*@Test
+    public void testLoadGrass() {
+        IntkeyContext context = new IntkeyContext(null);
+        context.newDataSetFile("C:\\Users\\ChrisF\\Documents\\grasses\\intkey.ini");
+        
+        IntkeyDataset ds = context.getDataset();
+        
+        System.out.println("foo");
+    }*/
+}
