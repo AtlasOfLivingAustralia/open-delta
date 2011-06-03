b888857b2f5fe39b09ef9f84542803dc40401e47
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-06-03 00:28:11 +0000

    Some unit tests for specimen character availability
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@715 dd429c29-c832-59a0-e864-46e68368aa0b

--- intkey/src/test/java/au/org/ala/delta/inkey/model/UseDirectiveTest.java
+++ intkey/src/test/java/au/org/ala/delta/inkey/model/UseDirectiveTest.java
@@ -19,61 +19,154 @@ import au.org.ala.delta.model.RealCharacter;
 import au.org.ala.delta.model.UnorderedMultiStateCharacter;
 
 public class UseDirectiveTest extends TestCase {
-    
+
+    /**
+     * Test that when a character is used, values are set for its controlling
+     * characters (when the NONAUTOMATIC CONTROLLING CHARACTERS and USE
+     * CONTROLLING CHARACTERS FIRST directives are not used)
+     * 
+     * @throws Exception
+     */
     @Test
     public void testControllingCharactersSet() throws Exception {
         URL initFileUrl = getClass().getResource("/dataset/controlling_characters_simple/intkey.ink");
         IntkeyContext context = new IntkeyContext(null);
         context.newDataSetFile(new File(initFileUrl.toURI()).getAbsolutePath());
-        
+
         IntkeyDataset ds = context.getDataset();
-        
+
         UnorderedMultiStateCharacter charSeedPresence = (UnorderedMultiStateCharacter) ds.getCharacter(2);
         UnorderedMultiStateCharacter charSeedInShell = (UnorderedMultiStateCharacter) ds.getCharacter(3);
         RealCharacter charAvgThickness = (RealCharacter) ds.getCharacter(4);
-        
+
         IntkeyDirectiveInvocation invoc = new UseDirective().doProcess(context, "4,1");
         context.executeDirective(invoc);
-        
+
         Specimen specimen = context.getSpecimen();
-        
+
         MultiStateValue charSeedPresenceValue = (MultiStateValue) specimen.getValueForCharacter(charSeedPresence);
         assertEquals(1, charSeedPresenceValue.getStateValues().size());
         assertEquals(1, (int) charSeedPresenceValue.getStateValues().get(0));
-        
+
         MultiStateValue charSeedInShellValue = (MultiStateValue) specimen.getValueForCharacter(charSeedInShell);
         assertEquals(1, charSeedInShellValue.getStateValues().size());
-        assertEquals(1, (int) charSeedInShellValue.getStateValues().get(0));        
-        
+        assertEquals(1, (int) charSeedInShellValue.getStateValues().get(0));
+
         RealValue charAvgThicknessValue = (RealValue) specimen.getValueForCharacter(charAvgThickness);
         assertEquals(new FloatRange(1.0, 1.0), charAvgThicknessValue.getRange());
     }
-    
+
+    /**
+     * Test that when the value of a character is changed, the values set for
+     * any dependent characters are removed from the specimen.
+     * 
+     * @throws Exception
+     */
     @Test
     public void testDependentCharactersRemoved() throws Exception {
         URL initFileUrl = getClass().getResource("/dataset/controlling_characters_simple/intkey.ink");
         IntkeyContext context = new IntkeyContext(null);
         context.newDataSetFile(new File(initFileUrl.toURI()).getAbsolutePath());
-        
+
         IntkeyDataset ds = context.getDataset();
-        
+
         UnorderedMultiStateCharacter charSeedPresence = (UnorderedMultiStateCharacter) ds.getCharacter(2);
         UnorderedMultiStateCharacter charSeedInShell = (UnorderedMultiStateCharacter) ds.getCharacter(3);
         RealCharacter charAvgThickness = (RealCharacter) ds.getCharacter(4);
-        
+
         IntkeyDirectiveInvocation invoc = new UseDirective().doProcess(context, "4,1");
         context.executeDirective(invoc);
-        
+
         IntkeyDirectiveInvocation invoc2 = new UseDirective().doProcess(context, "/M 2,2");
         context.executeDirective(invoc2);
-        
+
         Specimen specimen = context.getSpecimen();
-        
+
         MultiStateValue charSeedPresenceValue = (MultiStateValue) specimen.getValueForCharacter(charSeedPresence);
         assertEquals(1, charSeedPresenceValue.getStateValues().size());
         assertEquals(2, (int) charSeedPresenceValue.getStateValues().get(0));
-        
+
         assertFalse(specimen.hasValueFor(charSeedInShell));
         assertFalse(specimen.hasValueFor(charAvgThickness));
     }
+
+    /**
+     * Test that when a character is made inapplicable my multiple controlling
+     * characters, it says marked as inapplicable until the values of all
+     * controlling characters are deleted or changed such that the character is
+     * no longer inapplicable
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testAvailabilityMultipleControllingCharacters() throws Exception {
+        URL initFileUrl = getClass().getResource("/dataset/controlling_characters_multiple_controlling/intkey.ink");
+        IntkeyContext context = new IntkeyContext(null);
+        context.newDataSetFile(new File(initFileUrl.toURI()).getAbsolutePath());
+
+        IntkeyDataset ds = context.getDataset();
+
+        au.org.ala.delta.model.Character charThree = ds.getCharacter(3);
+
+        assertTrue(context.getSpecimen().getAvailableCharacters().contains(charThree));
+
+        IntkeyDirectiveInvocation invoc = new UseDirective().doProcess(context, "1,2");
+        context.executeDirective(invoc);
+
+        assertFalse(context.getSpecimen().getAvailableCharacters().contains(charThree));
+
+        IntkeyDirectiveInvocation invoc2 = new UseDirective().doProcess(context, "2,2");
+        context.executeDirective(invoc2);
+
+        assertFalse(context.getSpecimen().getAvailableCharacters().contains(charThree));
+
+        IntkeyDirectiveInvocation invoc3 = new UseDirective().doProcess(context, "/M 2,1");
+        context.executeDirective(invoc3);
+
+        assertFalse(context.getSpecimen().getAvailableCharacters().contains(charThree));
+
+        IntkeyDirectiveInvocation invoc4 = new UseDirective().doProcess(context, "/M 1,1");
+        context.executeDirective(invoc4);
+
+        assertTrue(context.getSpecimen().getAvailableCharacters().contains(charThree));
+
+        // TODO do restart and follow same process but delete values for
+        // characters 1 and 2
+        // instead of changing their values
+    }
+    
+    @Test
+    public void testAvailabilityControlHierarchy() throws Exception {
+        URL initFileUrl = getClass().getResource("/dataset/controlling_characters_simple/intkey.ink");
+        IntkeyContext context = new IntkeyContext(null);
+        context.newDataSetFile(new File(initFileUrl.toURI()).getAbsolutePath());
+
+        IntkeyDataset ds = context.getDataset();
+
+        UnorderedMultiStateCharacter charSeedPresence = (UnorderedMultiStateCharacter) ds.getCharacter(2);
+        UnorderedMultiStateCharacter charSeedInShell = (UnorderedMultiStateCharacter) ds.getCharacter(3);
+        RealCharacter charAvgThickness = (RealCharacter) ds.getCharacter(4);
+
+        IntkeyDirectiveInvocation invoc = new UseDirective().doProcess(context, "4,1");
+        context.executeDirective(invoc);
+
+        Specimen specimen = context.getSpecimen();
+        
+        IntkeyDirectiveInvocation invoc2 = new UseDirective().doProcess(context, "/M 2,2");
+        context.executeDirective(invoc2);        
+        
+        assertFalse(specimen.getAvailableCharacters().contains(charSeedInShell));
+        assertFalse(specimen.getAvailableCharacters().contains(charAvgThickness));
+        assertFalse(specimen.hasValueFor(charSeedInShell));
+        assertFalse(specimen.hasValueFor(charAvgThickness));
+        
+        IntkeyDirectiveInvocation invoc3 = new UseDirective().doProcess(context, "/M 2,1");
+        context.executeDirective(invoc3);
+        
+        assertTrue(specimen.getAvailableCharacters().contains(charSeedInShell));
+        assertTrue(specimen.getAvailableCharacters().contains(charAvgThickness));
+        assertFalse(specimen.hasValueFor(charSeedInShell));
+        assertFalse(specimen.hasValueFor(charAvgThickness));
+    }
+
 }
