6cde49e96097fc9cc31b95a42037901ec14204d7
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-06-10 00:55:13 +0000

    Commit empty test for restart directive so that the test class does not fail in the automated build
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@750 dd429c29-c832-59a0-e864-46e68368aa0b

--- intkey/src/test/java/au/org/ala/delta/inkey/model/RestartDirectiveTest.java
+++ intkey/src/test/java/au/org/ala/delta/inkey/model/RestartDirectiveTest.java
@@ -1,7 +1,14 @@
 package au.org.ala.delta.inkey.model;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class RestartDirectiveTest extends TestCase {
+    
+    @Test
+    public void testRestartDirective() {
+        
+    }
 
 }
