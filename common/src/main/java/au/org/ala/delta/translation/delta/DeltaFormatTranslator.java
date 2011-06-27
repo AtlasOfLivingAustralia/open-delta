19b1a69a42fcaa6d47f1787a0463b9726afb3439
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-06-27 01:24:52 +0000

    First cut of TRANSLATE INTO DELTA FORMAT.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@835 dd429c29-c832-59a0-e864-46e68368aa0b

--- common/src/main/java/au/org/ala/delta/translation/delta/DeltaFormatTranslator.java
+++ common/src/main/java/au/org/ala/delta/translation/delta/DeltaFormatTranslator.java
@@ -5,55 +5,93 @@ import org.apache.commons.lang.StringUtils;
 import au.org.ala.delta.DeltaContext;
 import au.org.ala.delta.model.Attribute;
 import au.org.ala.delta.model.Item;
+import au.org.ala.delta.model.MultiStateAttribute;
 import au.org.ala.delta.model.format.ItemFormatter;
 import au.org.ala.delta.translation.AbstractDataSetTranslator;
 import au.org.ala.delta.translation.Printer;
+import au.org.ala.delta.translation.attribute.AttributeParser;
+import au.org.ala.delta.translation.attribute.ParsedAttribute;
 import au.org.ala.delta.translation.attribute.ParsedAttribute.Values;
-import au.org.ala.delta.translation.naturallanguage.NaturalLanguageDataSetFilter;
 
+/**
+ * The DeltaFormatTranslator can reformat the output of the 
+ * ITEM DESCRIPTIONS and CHARACTER LIST directives.  It is used when the
+ * TRANSLATE INTO DELTA FORMAT directive is specified.
+ */
 public class DeltaFormatTranslator extends AbstractDataSetTranslator {
 
 	private Printer _printer;
 	private ItemFormatter _itemFormatter;
+	private AttributeParser _parser;
 	
 	public DeltaFormatTranslator(DeltaContext context, Printer printer) {
-		super(context, new NaturalLanguageDataSetFilter(context));
+		super(context, new DeltaFormatDataSetFilter(context));
 		
-		_printer = new Printer(context.getPrintStream(), context.getPrintWidth());
-		_itemFormatter = new ItemFormatter();
+		_printer = printer;
+		_itemFormatter = new ItemFormatter(false, false, false, false, false);
+		 _parser = new AttributeParser();
 	}
 	
 	@Override
 	public void beforeFirstItem() {
-		outputLine("* ITEM DESCRIPTIONS");
+		outputLine("*ITEM DESCRIPTIONS");
+		_printer.writeBlankLines(1, 0);
 	}
 
 	@Override
 	public void beforeItem(Item item) {
-		output("#");
-		output(_itemFormatter.formatItemDescription(item));
-		output("/");
-		_printer.printBufferLine();
+		StringBuilder itemDescription = new StringBuilder();
+		itemDescription.append("# ");
+		itemDescription.append(_itemFormatter.formatItemDescription(item));
+		itemDescription.append("/");
+		outputLine(itemDescription.toString());
 	}
 
 	@Override
 	public void afterItem(Item item) {
 		_printer.printBufferLine();
-		_printer.writeBlankLines(1, 5);
+		outputLine("");		
 	}
 
 	@Override
 	public void beforeAttribute(Attribute attribute) {
+		
 		StringBuilder attributeValue = new StringBuilder();
 		
 		au.org.ala.delta.model.Character character = attribute.getCharacter();
 		attributeValue.append(Integer.toString(character.getCharacterId()));
-		attributeValue.append(",");
-		String value = attribute.getValueAsString();
+		
+		String value = getAttributeValue(attribute);
 		if (StringUtils.isNotEmpty(value)) {
-			attributeValue.append(value);
+			ParsedAttribute parsedAttribute = _parser.parse(value);
+			String charComment = parsedAttribute.getCharacterComment();
+			attributeValue.append(charComment);
+			
+			if (!value.equals(charComment)) {
+				attributeValue.append(",");
+				attributeValue.append(value.substring(charComment.length()));
+			}
+			
 		}
+		else {
+			attributeValue.append(",");
+		}
+		// This is here for CONFOR compatibility, which makes testing
+		// easier.
+		attributeValue.append(" ");
+		
 		output(attributeValue.toString());
+	}
+
+	private String getAttributeValue(Attribute attribute) {
+		String value = attribute.getValueAsString();
+		if (attribute instanceof MultiStateAttribute) {
+            MultiStateAttribute msAttr = (MultiStateAttribute) attribute;
+            if (msAttr.isImplicit()) {
+                value = Integer.toString(msAttr.getImplicitValue());
+            }
+        }
+		return value;
 	}
 
 	@Override
@@ -74,7 +112,7 @@ public class DeltaFormatTranslator extends AbstractDataSetTranslator {
 	}
 	
 	private void outputLine(String value) {
-		output(value);
+		_printer.writeJustifiedText(value, -1);
 		_printer.printBufferLine();
 	}
 }
--- common/src/test/java/au/org/ala/delta/translation/delta/DeltaFormatTranslatorTest.java
+++ common/src/test/java/au/org/ala/delta/translation/delta/DeltaFormatTranslatorTest.java
@@ -24,9 +24,9 @@ public class DeltaFormatTranslatorTest extends TranslatorTest {
 		
 		_bytes = new ByteArrayOutputStream();
 		PrintStream pout = new PrintStream(_bytes, false, "UTF-8");
-		_printer = new Printer(pout, 78);
+		_printer = new Printer(pout, 80);
 		_context = new DeltaContext();
-		_printer = new Printer(_context.getPrintStream(), _context.getPrintWidth());
+		
 		_dataSetTranslator = new DeltaFormatTranslator(_context, _printer);
 	}
 	
@@ -34,5 +34,52 @@ public class DeltaFormatTranslatorTest extends TranslatorTest {
 		initialiseContext(DEFAULT_DATASET_PATH);
 		_dataSetTranslator.translate();
 		
+		checkResult("/dataset/sample/expected_results/deltaformatitems.txt");
+		
+	}
+
+	/**
+	 * Checks the result of the translation is identical to the contents of the supplied file.
+	 *
+	 * @param expectedResultsFileName the name of the file - the path "/dataset/simple/expected_results/" is 
+	 * prepended before loading the file contents.
+	 * @param vide true if we are checking the Vide dataset - we do a few things a bit differently
+	 * to CONFOR that I am taking into account.
+	 * @throws Exception if there is an error reading the file.
+	 */
+	protected void checkResult(String expectedResultsFileName) throws Exception {
+		
+		String expectedResults = classLoaderPathToString(expectedResultsFileName);
+		
+		boolean dosEol = expectedResults.contains("\r\n");
+		String expectedLineSeparator = "\n";
+		if (dosEol) {
+			expectedLineSeparator = "\r\n";
+		}
+		String lineSeparator = System.getProperty("line.separator");
+		if (!lineSeparator.equals(expectedLineSeparator)) {
+			expectedResults = expectedResults.replaceAll(expectedLineSeparator, lineSeparator);
+		}
+		
+		expectedResults = expectedResults.trim();
+		String actualResults = actualResults().trim();
+		
+		// Replace windows code page chars
+		
+		actualResults = actualResults.replaceAll("\\\\emdash\\{\\}", "\u2014");
+		actualResults = actualResults.replaceAll("\\\\endash\\{\\}", "\u2013");
+		
+		
+		// This is here because I keep getting bitten by end of line issues and the test failure
+		// comparison editor doesn't display them.
+		for (int i=0; i<expectedResults.length() && i<actualResults.length(); i++) {
+			if (expectedResults.charAt(i) != actualResults.charAt(i)) {
+				System.out.println("First wrong character @ position "+i);
+				System.out.println("Expected: "+Integer.toHexString((int)expectedResults.charAt(i))+", found: "+Integer.toHexString((int)actualResults.charAt(i)));
+				break;
+			}
+		}
+		
+		assertEquals(expectedResults, actualResults);
 	}
 }
