fef8d976017ba43d381b0557fef69cf00e055713
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-07-07 06:52:27 +0000

    Helper class for building RTF documents
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@893 dd429c29-c832-59a0-e864-46e68368aa0b

---
+++ common/src/main/java/au/org/ala/delta/rtf/RTFBuilder.java
@@ -0,0 +1,56 @@
+package au.org.ala.delta.rtf;
+
+public class RTFBuilder {
+
+    private StringBuilder _strBuilder;
+
+    // by default, the indent width is 720, which is the default tab size.
+    private int _indentWidth = 720;
+    private int _currentIndent = 0;
+
+    private RTFBuilder() {
+        _strBuilder = new StringBuilder();
+    }
+
+    public String toString() {
+        return _strBuilder.toString();
+    }
+
+    public void setIdentWidth(int twips) {
+        _indentWidth = twips;
+    }
+
+    public void startDocument() {
+        _strBuilder.append("{\\rtf1\\ansi\\deff0 {\fonttbl {\f0 Times New Roman;}}");
+        _strBuilder.append("{\\colortbl ;\\red255\\green0\\blue0;}");
+    }
+
+    public void endDocument() {
+        _strBuilder.append("}");
+    }
+
+    public void startParagraph() {
+        _strBuilder.append("\\pard");
+    }
+
+    public void endParagraph() {
+        _strBuilder.append("\\par");
+    }
+
+    public void increaseIndent() {
+        _currentIndent++;
+        int indentTwips = _currentIndent * _indentWidth;
+        _strBuilder.append(String.format("\\li%s", indentTwips));
+    }
+
+    public void decreaseIndent() {
+        _currentIndent--;
+        int indentTwips = _currentIndent * _indentWidth;
+        _strBuilder.append(String.format("\\li%s", indentTwips));
+    }
+    
+    public void appendText(String str) {
+        _strBuilder.append(str);
+    }
+
+}
