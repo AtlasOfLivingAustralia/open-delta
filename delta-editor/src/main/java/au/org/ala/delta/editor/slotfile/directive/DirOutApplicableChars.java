f964a2460e11aa76f845797e1c8fadb07a48003a
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-07-08 04:24:40 +0000

    Implemented export of applicable characters directive.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@901 dd429c29-c832-59a0-e864-46e68368aa0b

--- delta-editor/src/main/java/au/org/ala/delta/editor/slotfile/directive/DirOutApplicableChars.java
+++ delta-editor/src/main/java/au/org/ala/delta/editor/slotfile/directive/DirOutApplicableChars.java
@@ -14,13 +14,118 @@
  ******************************************************************************/
 package au.org.ala.delta.editor.slotfile.directive;
 
-import org.apache.commons.lang.NotImplementedException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class DirOutApplicableChars implements DirectiveFunctor {
+import au.org.ala.delta.model.Character;
+import au.org.ala.delta.model.CharacterDependency;
+import au.org.ala.delta.model.DeltaDataSet;
+import au.org.ala.delta.model.MultiStateCharacter;
+
+/**
+ * Exports the APPLICABLE CHARACTERS directive.
+ */
+public class DirOutApplicableChars extends AbstractDirOutFunctor {
 
 	@Override
-	public void process(DirectiveInOutState state) {
-		throw new NotImplementedException();
-	}
+	public void writeDirectiveArguments(DirectiveInOutState state) {
+		// YUCK! To get this to work, we must first gather together ALL the
+		// controlling
+		// attributes under the control of a given character, then accumulate a
+		// list of
+		// all the characters under the control of this controlling character,
+		// via any
+		// of its various controlling attributes. Then go through each state of
+		// the controlling
+		// character and determine which of the controlled characters are NOT
+		// under it's
+		// control...
 
+		DeltaDataSet dataSet = state.getDataSet();
+
+		for (int i = 1; i <= dataSet.getNumberOfCharacters(); i++) {
+			Character character = dataSet.getCharacter(i);
+
+			if (character.getDependentCharacters().size() == 0)
+				continue;
+
+			MultiStateCharacter multiStateChar = (MultiStateCharacter) character;
+			int nStates = multiStateChar.getNumberOfStates();
+			if (nStates == 0)
+				throw new RuntimeException(" TDirInOutEx(ED_INTERNAL_ERROR)");
+
+			// std::vector<TUniIdSet> stateLists(nStates); // Creates a vector
+			// of sets; one set for each state
+			// std::vector<TUniIdVector> revStateLists(nStates); // And a
+			// corresponding vector of vectors
+			List<Set<Integer>> stateLists = new ArrayList<Set<Integer>>();
+			for (int tmp = 0; tmp < nStates; tmp++) {
+				stateLists.add(new HashSet<Integer>());
+			}
+			Set<Integer> allDeps = new HashSet<Integer>();
+
+			List<CharacterDependency> contAttrs = character
+					.getDependentCharacters();
+
+			for (CharacterDependency dependency : contAttrs) {
+
+				List<Integer> states = dependency.getStatesAsList();
+				Set<Integer> controlled = dependency.getDependentCharacterIds();
+				for (int contCharNo : controlled) {
+
+					if (contCharNo == 0)
+						throw new RuntimeException(
+								"TDirInOutEx(ED_INTERNAL_ERROR)");
+					allDeps.add(contCharNo);
+					for (int stateNo : states) {
+
+						if (stateNo == 0 || stateNo > nStates)
+							throw new RuntimeException(
+									"TDirInOutEx(ED_INTERNAL_ERROR)");
+						stateLists.get(stateNo - 1).add(contCharNo);
+					}
+				}
+			}
+			// At this point, allDeps should contain the character numbers
+			// of all characters under the control of the current one
+			List<List<Integer>> revStateLists = new ArrayList<List<Integer>>();
+
+			for (int m = 0; m < nStates; ++m) {
+				Set<Integer> tmp = new HashSet<Integer>(allDeps);
+				tmp.removeAll(stateLists.get(m));
+				revStateLists.add(new ArrayList<Integer>(tmp));
+				Collections.sort(revStateLists.get(m));
+
+			}
+
+			// And now we should have, for each state, a sorted list of the
+			// character numbers of
+			// of all characters which it makes applicable.
+			for (int m = 0; m < nStates; ++m) {
+				if (revStateLists.get(m).size() > 0) {
+					_textBuffer.append(' ');
+					_textBuffer.append(i);
+					_textBuffer.append(',');
+					_textBuffer.append(m + 1);
+
+					// Search for any other states with the same set of
+					// applicable characters
+					for (int n = m + 1; n < nStates; ++n) {
+						if (revStateLists.get(m).equals(revStateLists.get(n))) {
+							_textBuffer.append('/');
+							_textBuffer.append(n + 1);
+							revStateLists.get(n).clear();
+						}
+					}
+					_textBuffer.append(':');
+					_textBuffer
+							.append(rangeToString(revStateLists.get(m), ':'));
+				}
+			}
+		}
+		writeLine(state, _textBuffer.toString());
+	}
 }
