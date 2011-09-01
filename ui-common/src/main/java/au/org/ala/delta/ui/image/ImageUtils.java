da0b2bd8e7afb3b4045872929daf7c09f5c3683b
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-09-01 23:43:22 +0000

    Modification to full-screen image display helper method. Avoids annoying effects in windows due to changing graphics mode.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@1140 dd429c29-c832-59a0-e864-46e68368aa0b

--- ui-common/src/main/java/au/org/ala/delta/ui/image/ImageUtils.java
+++ ui-common/src/main/java/au/org/ala/delta/ui/image/ImageUtils.java
@@ -2,8 +2,12 @@ package au.org.ala.delta.ui.image;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dialog.ModalityType;
+import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -11,6 +15,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import au.org.ala.delta.model.image.Image;
@@ -24,7 +30,8 @@ public class ImageUtils {
     // window if the last image
     // is currently being displayed.
     public static void displayImagesFullScreen(List<Image> images, ImageSettings imageSettings, Window applicationWindow) {
-        final Window w = new Window(applicationWindow);
+        final JFrame w = new JFrame(applicationWindow.getGraphicsConfiguration());
+        w.setUndecorated(true);
         w.setLayout(new BorderLayout());
 
         final MultipleImageViewer viewer = new MultipleImageViewer(imageSettings);
@@ -42,17 +49,30 @@ public class ImageUtils {
         panel.add(viewer);
         w.add(panel, BorderLayout.CENTER);
 
-        final GraphicsDevice gd = applicationWindow.getGraphicsConfiguration().getDevice();
-
         w.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 if (viewer.atLastImage()) {
                     w.setVisible(false);
-                    w.dispose();
-                    gd.setFullScreenWindow(null);
                 } else {
                     viewer.nextImage();
                 }
+            }
+        });
+        
+        w.addKeyListener(new KeyListener() {
+            
+            @Override
+            public void keyTyped(KeyEvent e) {
+                w.setVisible(false);
+                w.dispose();
+            }
+            
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+            
+            @Override
+            public void keyPressed(KeyEvent e) {
             }
         });
 
@@ -72,13 +92,15 @@ public class ImageUtils {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
                     w.setVisible(false);
-                    w.dispose();
-                    gd.setFullScreenWindow(null);
                 }
             }
         });
 
-        gd.setFullScreenWindow(w);
+        Rectangle r = applicationWindow.getGraphicsConfiguration().getBounds();
+        w.setLocation(r.x, r.y);
+                       
+        w.setSize(new Dimension(r.width, r.height));        
+        w.setVisible(true);
     }
 
 }
