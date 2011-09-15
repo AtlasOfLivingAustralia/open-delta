423ffe8f5103914103c15bc2b54e0e6ebafb847b
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-09-15 02:30:44 +0000

    Implemented CHARACTERS report
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@1231 dd429c29-c832-59a0-e864-46e68368aa0b

--- intkey/src/main/java/au/org/ala/delta/intkey/directives/invocation/CharactersDirectiveInvocation.java
+++ intkey/src/main/java/au/org/ala/delta/intkey/directives/invocation/CharactersDirectiveInvocation.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import au.org.ala.delta.intkey.model.IntkeyContext;
 import au.org.ala.delta.model.Character;
+import au.org.ala.delta.model.MultiStateCharacter;
+import au.org.ala.delta.model.NumericCharacter;
+import au.org.ala.delta.model.format.CharacterFormatter;
+import au.org.ala.delta.model.format.ItemFormatter;
+import au.org.ala.delta.rtf.RTFBuilder;
 
 public class CharactersDirectiveInvocation implements IntkeyDirectiveInvocation {
-    
+
     private List<Character> _characters;
 
     public void setCharacters(List<Character> characters) {
@@ -15,9 +20,34 @@ public class CharactersDirectiveInvocation implements IntkeyDirectiveInvocation 
 
     @Override
     public boolean execute(IntkeyContext context) {
-        System.out.println(_characters.toString());
+        CharacterFormatter characterFormatter = new CharacterFormatter(true, false, false, true, true);
+
+        RTFBuilder builder = new RTFBuilder();
+        builder.startDocument();
+
+        for (Character ch : _characters) {
+            builder.appendText(characterFormatter.formatCharacterDescription(ch));
+            builder.increaseIndent();
+
+            if (ch instanceof MultiStateCharacter) {
+                MultiStateCharacter msChar = (MultiStateCharacter) ch;
+                for (int i = 0; i < msChar.getNumberOfStates(); i++) {
+                    builder.appendText(characterFormatter.formatState(msChar, i + 1));
+                }
+            } else if (ch instanceof NumericCharacter<?>) {
+                NumericCharacter<?> numChar = (NumericCharacter<?>) ch;
+                builder.appendText(characterFormatter.formatUnits(numChar));
+            }
+
+            builder.decreaseIndent();
+            builder.appendText("");
+        }
         
-        //TODO generate and show report
+        builder.endDocument();
+
+        context.getUI().displayRTFReport(builder.toString(), "Characters");
+
+        // TODO generate and show report
         return true;
     }
 
