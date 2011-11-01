4643254f524084f6c2715697c902699254fb6e73
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2011-11-01 06:35:10 +0000

    Added stub main class to Key project
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@1472 dd429c29-c832-59a0-e864-46e68368aa0b

---
+++ key/src/main/java/au/org/ala/delta/key/Key.java
@@ -0,0 +1,12 @@
+package au.org.ala.delta.key;
+
+public class Key {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("KEY!");
+    }
+
+}
