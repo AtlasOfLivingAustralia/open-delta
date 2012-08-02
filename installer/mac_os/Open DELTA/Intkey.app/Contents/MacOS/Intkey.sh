e27a56ad32ff2ddb0c60ac56be6070577bee7942
chris.godwin.ala@gmail.com <chris.godwin.ala@gmail.com@dd429c29-c832-59a0-e864-46e68368aa0b>
2012-08-02 23:10:49 +0000

    Fixed a bug in the Mac installer.
    git-svn-id: https://open-delta.googlecode.com/svn/trunk@1926 dd429c29-c832-59a0-e864-46e68368aa0b

--- installer/mac_os/Open DELTA/Intkey.app/Contents/MacOS/Intkey.sh
+++ installer/mac_os/Open DELTA/Intkey.app/Contents/MacOS/Intkey.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export SCRIPT_DIR=$DIR/../../../../.open-delta/bin/
+export SCRIPT_DIR=$DIR/../../../.open-delta/bin/
 cd "$SCRIPT_DIR"
 chmod u+x Intkey.sh
 ./Intkey.sh
--- installer/mac_os/updateDmg.sh
+++ installer/mac_os/updateDmg.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+VERSION=1.0-RC3
 cp Open\ DELTA.dmg /tmp
 hdiutil attach /tmp/Open\ DELTA.dmg
 cd ../../target
-unzip open-delta-1.0-RC2-mac_os_bundle.zip
-cd open-delta-1.0-RC2
-cp -R .open-delta /Volumes/Open\ DELTA/
+unzip open-delta-$VERSION-mac_os_bundle.zip
+cd open-delta-$VERSION
 cp -R Open\ DELTA /Volumes/Open\ DELTA/
+cp -R .open-delta /Volumes/Open\ DELTA/Open\ DELTA/
 hdiutil detach /Volumes/Open\ DELTA
 hdiutil convert -format UDCO -o ../Open\ DELTA.dmg /tmp/Open\ DELTA.dmg
 
