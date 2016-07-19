ed7b9cba8a32653fb9c54b65f66151e138a80561
chrisala <chris.godwin.ala@gmail.com>
2016-07-19 16:54:20 +1000

    Attempt to set java_home on mac os x

--- installer/mac_os/Open DELTA/Delta Editor.app/Contents/MacOS/DeltaEditor.sh
+++ installer/mac_os/Open DELTA/Delta Editor.app/Contents/MacOS/DeltaEditor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -z "$JAVA_HOME" ] ; then
+    export JAVA_HOME=`/usr/libexec/java_home`
+fi
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SCRIPT_DIR=$DIR/../../../.open-delta/bin/
 cd "$SCRIPT_DIR"
